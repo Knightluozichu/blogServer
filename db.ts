756e371f132b4129710f622475520e7dc8678290
luozichu <luozichu@bilibili.com>
2023-11-01 11:38:35 +0800

    feat mongodb task

--- db.ts
+++ db.ts
@@ -8,26 +8,49 @@ async function main() {
     // const allUsers = await prisma.user.findMany()
     // log(allUsers);
 
-    await prisma.user.create({
-        data: {
-          name: 'Rich',
-          email: 'hello@prisma.com',
-          posts: {
-            create: {
-              title: 'My first post',
-              body: 'Lots of really interesting stuff',
-              slug: 'my-first-post',
-            },
+    // await prisma.user.create({
+    //     data: {
+    //       name: 'Rich',
+    //       email: 'hello@prisma.com',
+    //       posts: {
+    //         create: {
+    //           title: 'My first post',
+    //           body: 'Lots of really interesting stuff',
+    //           slug: 'my-first-post',
+    //         },
+    //       },
+    //     },
+    //   })
+    
+    //   const allUsers = await prisma.user.findMany({
+    //     include: {
+    //       posts: true,
+    //     },
+    //   })
+    //   dir(allUsers, { depth: null })
+
+    await prisma.post.update({
+      where: {
+        slug: 'my-first-post',
+      },
+      data: {
+        comments: {
+          createMany: {
+            data: [
+              { comment: 'Great post!' },
+              { comment: "Can't wait to read more!" },
+            ],
           },
         },
-      })
-    
-      const allUsers = await prisma.user.findMany({
-        include: {
-          posts: true,
-        },
-      })
-      dir(allUsers, { depth: null })
+      },
+    })
+    const posts = await prisma.post.findMany({
+      include: {
+        comments: true,
+      },
+    })
+  
+    console.dir(posts, { depth: Infinity })
 }
 
 main()
---
+++ test.ts
@@ -0,0 +1,5 @@
+// mongod --port 27017 --dbpath "D:/program files/MongoDB/Server/7.0/data" --replSet myReplicaSet
+// mongosh --port 27017
+// rs.initiate()
+// rs.add("mongodb0.example.net:27018")
+// rs.add("mongodb1.example.net:27019")
