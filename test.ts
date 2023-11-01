4de688125bbabb17cc9b22aa8836c9054c56e31c
hellsing <luozichu@bilibili.com>
2023-11-01 20:25:28 +0800

    feat mongodb 副本集 test.js

--- test.ts
+++ test.ts
@@ -1,4 +1,5 @@
-// mongod --port 27017 --dbpath "D:/program files/MongoDB/Server/7.0/data" --replSet myReplicaSet
+
+// mongod --port 27017 --dbpath "F:/Learn/DB/MongoDB/Server/6.0/data" --replSet myReplicaSet
 // mongosh --port 27017
 // rs.initiate()
 // rs.add("mongodb0.example.net:27018")
